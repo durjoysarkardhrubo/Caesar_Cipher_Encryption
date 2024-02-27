f77730c271601679e0a7b35dd37eae440f7050c9
Durjoy Sarkar Dhrubo <155498741+DURJOY-EEE-KUET@users.noreply.github.com>
2024-02-27 23:45:14 +0600

    Add files via upload

---
+++ en.py
@@ -0,0 +1,17 @@
+
+def encrypt(text, shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            result += char
+    return result
+
+text = input("Enter text to encrypt: ")
+shift = 3
+encrypted_text = encrypt(text, shift)
+print("Encrypted text:", encrypted_text)
